cc4745a33fb47b8b916d7aad240a7aefa87401bb
TTBBKK <148713084+TTBBKK@users.noreply.github.com>
2024-02-04 17:05:30 +0800

    Update TagItemMini.js

--- themes/hexo/components/TagItemMini.js
+++ themes/hexo/components/TagItemMini.js
@@ -6,7 +6,7 @@ const TagItemMini = ({ tag, selected = false }) => {
       key={tag}
       href={selected ? '/' : `/tag/${encodeURIComponent(tag.name)}`}
       passHref
-      className={`cursor-pointer inline-block rounded hover:bg-null dark:hover:text-white  hover:text-gray-400 duration-200
+      className={`cursor-pointer inline-block border hover:bg-null dark:hover:text-white  hover:text-gray-400 duration-200
         mr-2 py-0.5 px-1 text-xs whitespace-nowrap 
          ${selected
         ? 'text-white dark:text-gray-300 bg-black dark:bg-black dark:hover:bg-gray-300'
